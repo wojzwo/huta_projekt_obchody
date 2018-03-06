8b72ebf2b24b05c1595fe66d1c0cbd566609e197
Thirr-PC\Thirr <thirrasch@gmail.com>
2018-03-06 23:31:55 +0100

    Added inserting and deleting chips; Added updating marks

--- SteelWorks_Utils/Model/DB_Employee.cs
+++ SteelWorks_Utils/Model/DB_Employee.cs
@@ -8,6 +8,7 @@ namespace SteelWorks_Utils.Model
 {
     public class DB_Employee
     {
+        /// <summary> This id is used for faster database lookup, THIS IS NOT CHIP ID </summary>
         public int id = -1;
         public string name;
         public string chipId;

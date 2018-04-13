6a919fd8c52e9eb05416144d5497430b022ccb4e
wz373286 <w.zwolinski2@student.uw.edu.pl>
2018-04-13 02:36:19 +0200

    Fixed routine once again

--- SteelWorks_Admin/View/RoutineListUserControl.cs
+++ SteelWorks_Admin/View/RoutineListUserControl.cs
@@ -90,8 +90,6 @@ namespace SteelWorks_Admin.View
 		}
 
 		private void addRoutineButton_Click(object sender, EventArgs e) {
-		    if (routineListBox.SelectedIndex < 0)
-		        return;
 
 			routineUC.set_routine(new DbRoutine()
 			{

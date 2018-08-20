48d1cfd4d20cd2e698c07e19fb3e369c37606a84
wz373286 <w.zwolinski2@student.uw.edu.pl>
2018-08-20 15:44:15 +0200

    Corrected Logo

--- SteelWorks_Worker/View/WorkerMainView.Designer.cs
+++ SteelWorks_Worker/View/WorkerMainView.Designer.cs
@@ -25,118 +25,124 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WorkerMainView));
-            this.dataRemoveOrLoadSelectionUserControl = new SteelWorks_Worker.View.DataRemoveOrLoadSelectionUserControl();
-            this.loadReaderUserControl = new SteelWorks_Worker.View.LoadReaderUserControl();
-            this.startUserControl = new SteelWorks_Worker.View.StartUserControl();
-            this.dataRemoveUserControl_ = new SteelWorks_Worker.View.DataRemoveUserControl();
-            this.dataRemoveSuccessUserControl_ = new SteelWorks_Worker.View.DataRemoveSuccessUserControl();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // dataRemoveOrLoadSelectionUserControl
-            // 
-            this.dataRemoveOrLoadSelectionUserControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WorkerMainView));
+			this.dataRemoveOrLoadSelectionUserControl = new SteelWorks_Worker.View.DataRemoveOrLoadSelectionUserControl();
+			this.loadReaderUserControl = new SteelWorks_Worker.View.LoadReaderUserControl();
+			this.startUserControl = new SteelWorks_Worker.View.StartUserControl();
+			this.dataRemoveUserControl_ = new SteelWorks_Worker.View.DataRemoveUserControl();
+			this.dataRemoveSuccessUserControl_ = new SteelWorks_Worker.View.DataRemoveSuccessUserControl();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataRemoveOrLoadSelectionUserControl
+			// 
+			this.dataRemoveOrLoadSelectionUserControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataRemoveOrLoadSelectionUserControl.BackColor = System.Drawing.SystemColors.Control;
-            this.dataRemoveOrLoadSelectionUserControl.Location = new System.Drawing.Point(12, 12);
-            this.dataRemoveOrLoadSelectionUserControl.Name = "dataRemoveOrLoadSelectionUserControl";
-            this.dataRemoveOrLoadSelectionUserControl.Size = new System.Drawing.Size(1240, 657);
-            this.dataRemoveOrLoadSelectionUserControl.TabIndex = 1;
-            this.dataRemoveOrLoadSelectionUserControl.Visible = false;
-            this.dataRemoveOrLoadSelectionUserControl.Load += new System.EventHandler(this.dataRemoveOrLoadSelectionUserControl_Load);
-            // 
-            // loadReaderUserControl
-            // 
-            this.loadReaderUserControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.dataRemoveOrLoadSelectionUserControl.BackColor = System.Drawing.SystemColors.Control;
+			this.dataRemoveOrLoadSelectionUserControl.Location = new System.Drawing.Point(12, 12);
+			this.dataRemoveOrLoadSelectionUserControl.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+			this.dataRemoveOrLoadSelectionUserControl.Name = "dataRemoveOrLoadSelectionUserControl";
+			this.dataRemoveOrLoadSelectionUserControl.Size = new System.Drawing.Size(1240, 657);
+			this.dataRemoveOrLoadSelectionUserControl.TabIndex = 1;
+			this.dataRemoveOrLoadSelectionUserControl.Visible = false;
+			this.dataRemoveOrLoadSelectionUserControl.Load += new System.EventHandler(this.dataRemoveOrLoadSelectionUserControl_Load);
+			// 
+			// loadReaderUserControl
+			// 
+			this.loadReaderUserControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.loadReaderUserControl.BackColor = System.Drawing.SystemColors.Control;
-            this.loadReaderUserControl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.loadReaderUserControl.Location = new System.Drawing.Point(12, 12);
-            this.loadReaderUserControl.Name = "loadReaderUserControl";
-            this.loadReaderUserControl.Size = new System.Drawing.Size(1240, 657);
-            this.loadReaderUserControl.TabIndex = 0;
-            this.loadReaderUserControl.Visible = false;
-            this.loadReaderUserControl.Load += new System.EventHandler(this.loadReaderUserControl_Load);
-            // 
-            // startUserControl
-            // 
-            this.startUserControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.loadReaderUserControl.BackColor = System.Drawing.SystemColors.Control;
+			this.loadReaderUserControl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.loadReaderUserControl.Location = new System.Drawing.Point(12, 12);
+			this.loadReaderUserControl.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+			this.loadReaderUserControl.Name = "loadReaderUserControl";
+			this.loadReaderUserControl.Size = new System.Drawing.Size(1240, 657);
+			this.loadReaderUserControl.TabIndex = 0;
+			this.loadReaderUserControl.Visible = false;
+			this.loadReaderUserControl.Load += new System.EventHandler(this.loadReaderUserControl_Load);
+			// 
+			// startUserControl
+			// 
+			this.startUserControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.startUserControl.BackColor = System.Drawing.SystemColors.Control;
-            this.startUserControl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.startUserControl.Location = new System.Drawing.Point(12, 12);
-            this.startUserControl.Name = "startUserControl";
-            this.startUserControl.Size = new System.Drawing.Size(1240, 657);
-            this.startUserControl.TabIndex = 0;
-            this.startUserControl.Load += new System.EventHandler(this.startUserControl1_Load_1);
-            // 
-            // dataRemoveUserControl_
-            // 
-            this.dataRemoveUserControl_.BackColor = System.Drawing.SystemColors.Control;
-            this.dataRemoveUserControl_.Location = new System.Drawing.Point(12, 12);
-            this.dataRemoveUserControl_.Name = "dataRemoveUserControl_";
-            this.dataRemoveUserControl_.Size = new System.Drawing.Size(1240, 657);
-            this.dataRemoveUserControl_.TabIndex = 2;
-            this.dataRemoveUserControl_.Visible = false;
-            // 
-            // dataRemoveSuccessUserControl_
-            // 
-            this.dataRemoveSuccessUserControl_.BackColor = System.Drawing.SystemColors.Control;
-            this.dataRemoveSuccessUserControl_.Location = new System.Drawing.Point(12, 12);
-            this.dataRemoveSuccessUserControl_.Name = "dataRemoveSuccessUserControl_";
-            this.dataRemoveSuccessUserControl_.Size = new System.Drawing.Size(1240, 657);
-            this.dataRemoveSuccessUserControl_.TabIndex = 3;
-            this.dataRemoveSuccessUserControl_.Visible = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox2.BackgroundImage = global::SteelWorks_Worker.Properties.Resources.logo_hw;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(14, 14);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(93, 79);
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox1.BackgroundImage = global::SteelWorks_Worker.Properties.Resources.acelor;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(1130, 14);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(120, 79);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
-            // WorkerMainView
-            // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.dataRemoveOrLoadSelectionUserControl);
-            this.Controls.Add(this.loadReaderUserControl);
-            this.Controls.Add(this.startUserControl);
-            this.Controls.Add(this.dataRemoveUserControl_);
-            this.Controls.Add(this.dataRemoveSuccessUserControl_);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "WorkerMainView";
-            this.Text = "Obchody - Pracownik";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WorkerMainView_FormClosed);
-            this.Load += new System.EventHandler(this.BeforeLoad);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+			this.startUserControl.BackColor = System.Drawing.SystemColors.Control;
+			this.startUserControl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.startUserControl.Location = new System.Drawing.Point(12, 12);
+			this.startUserControl.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+			this.startUserControl.Name = "startUserControl";
+			this.startUserControl.Size = new System.Drawing.Size(1240, 657);
+			this.startUserControl.TabIndex = 0;
+			this.startUserControl.Load += new System.EventHandler(this.startUserControl1_Load_1);
+			// 
+			// dataRemoveUserControl_
+			// 
+			this.dataRemoveUserControl_.BackColor = System.Drawing.SystemColors.Control;
+			this.dataRemoveUserControl_.Location = new System.Drawing.Point(12, 12);
+			this.dataRemoveUserControl_.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+			this.dataRemoveUserControl_.Name = "dataRemoveUserControl_";
+			this.dataRemoveUserControl_.Size = new System.Drawing.Size(1240, 657);
+			this.dataRemoveUserControl_.TabIndex = 2;
+			this.dataRemoveUserControl_.Visible = false;
+			// 
+			// dataRemoveSuccessUserControl_
+			// 
+			this.dataRemoveSuccessUserControl_.BackColor = System.Drawing.SystemColors.Control;
+			this.dataRemoveSuccessUserControl_.Location = new System.Drawing.Point(12, 12);
+			this.dataRemoveSuccessUserControl_.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+			this.dataRemoveSuccessUserControl_.Name = "dataRemoveSuccessUserControl_";
+			this.dataRemoveSuccessUserControl_.Size = new System.Drawing.Size(1240, 657);
+			this.dataRemoveSuccessUserControl_.TabIndex = 3;
+			this.dataRemoveSuccessUserControl_.Visible = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+			this.pictureBox2.BackgroundImage = global::SteelWorks_Worker.Properties.Resources.logo_hw;
+			this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox2.Location = new System.Drawing.Point(14, 14);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(93, 79);
+			this.pictureBox2.TabIndex = 7;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+			this.pictureBox1.BackgroundImage = global::SteelWorks_Worker.Properties.Resources.acelor;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+			this.pictureBox1.Location = new System.Drawing.Point(1130, 14);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(120, 79);
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
+			// 
+			// WorkerMainView
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.DarkGray;
+			this.ClientSize = new System.Drawing.Size(1264, 681);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.dataRemoveOrLoadSelectionUserControl);
+			this.Controls.Add(this.loadReaderUserControl);
+			this.Controls.Add(this.startUserControl);
+			this.Controls.Add(this.dataRemoveUserControl_);
+			this.Controls.Add(this.dataRemoveSuccessUserControl_);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "WorkerMainView";
+			this.Text = "Obchody - Pracownik";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WorkerMainView_FormClosed);
+			this.Load += new System.EventHandler(this.BeforeLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
